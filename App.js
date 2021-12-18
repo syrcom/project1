1637dc93ac55a051e8b5af8d5666c16750f17a40
jackson <jackson@live.com.sg>
2021-12-18 11:58:15 +0800

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, FlatList, SafeAreaView } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello Wosrld</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const names = [
+  "Aaran",
+  "Aaren",
+  "Aarez",
+  "Aarman",
+  "Aaron",
+  "Aaron-James",
+  "Aarron",
+  "Aaryan",
+  "Aaryn",
+  "Aayan",
+  "Aazaan",
+  "Abaan",
+  "Abbas",
+  "Abdallah",
+  "Abdalroof",
+  "Abdihakim",
+  "Abdirahman",
+  "Abdisalam",
+  "Abdul",
+  "Abdul-Aziz",
+  "Abdulbasir",
+  "Abdulkadir",
+  "Abdulkarem",
+];
+
+const nameAsObjects = names.map((item) => {
+return {
+name: item,
 }
 
+}
+
+)
+
+export default function App() {
+ const renderName = ({ item }) => {
+return <Text>{item.name}</Text>;
+ };
+ return <FlatList data={nameAsObjects} renderItem={renderName}
+ keyExtractor={(item) => item.name}></FlatList>
+};
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container: {
+alignItems:"center",
+backgroundColor: "#fff",
+justifyContent: "center",
+},
+
 });
+
